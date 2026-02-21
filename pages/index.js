dfe336efaa0d95a64cc01e2160f4912ee39837fa
이민준 <115701381+eeinun@users.noreply.github.com>
2026-02-21 16:33:37 +0000

    Fix nicovideo redirect logic to assign target URL instead of immediate navigation

--- pages/index.js
+++ pages/index.js
@@ -81,7 +81,7 @@ export default function Home({ metaData, targetUrl }) {
     }
     else if (target.includes("nicovideo")) {
         const video_id = target.split("/").pop().split("?")[0];
-        window.location.href = `intent://video/${video_id}#Intent;scheme=nicovideo;package=jp.nicovideo.android;end`;
+        targetUrl = `intent://video/${video_id}#Intent;scheme=nicovideo;package=jp.nicovideo.android;end`;
     }
     // 그 외의 경우 (선택 사항: 그냥 해당 링크로 이동)
     else {
